ecc32934dcef3ea8712687eedb36c1f7aaa20efa
etecdeboramaia-lgtm <etec.deboramaia@gmail.com>
2026-02-26 09:06:49 -0300

    terceira aula pam

--- Lanchonete.dpr
+++ Lanchonete.dpr
@@ -5,7 +5,8 @@ uses
   FMX.Forms,
   uacesso in 'uacesso.pas' {frmacesso},
   umodulo in 'umodulo.pas' {dm: TDataModule},
-  uiuusuario in 'uiuusuario.pas' {frmiuusuario};
+  uiuusuario in 'uiuusuario.pas' {frmiuusuario},
+  ulistausuario in 'ulistausuario.pas' {frmlistausuario};
 
 {$R *.res}
 
@@ -14,5 +15,6 @@ begin
   Application.CreateForm(Tfrmiuusuario, frmiuusuario);
   Application.CreateForm(Tfrmacesso, frmacesso);
   Application.CreateForm(Tdm, dm);
+  Application.CreateForm(Tfrmlistausuario, frmlistausuario);
   Application.Run;
 end.
